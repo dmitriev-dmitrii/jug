cb36be2d70cbc96643113459fb3e5d47898bf541
Dmitriev-Dmitrii <dmitrievdmitrii@mail.com>
2021-02-21 01:13:29 +0300

    modals bug fixed
    modals bug fixed

---
+++ app/js/main — копия.js
@@ -0,0 +1,375 @@
+const slider = document.querySelector('.swiper-container');
+
+const mySwiper = new Swiper(slider, {
+	slidesPerView: 1,
+	spaceBetween: 20,
+	loop: true,
+	lazy: {
+		loadPrevNext: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
+
+const hideBtn = document.querySelectorAll('.hide__btn');
+
+hideBtn.forEach(btn => {
+
+
+let itemList= Array.from(btn.previousElementSibling);
+
+itemList.forEach(e => {
+
+	e.style.cssText = 'overflow: hidden;'
+
+});
+
+let itemListChilds= Array.from(btn.previousElementSibling.children);
+let hideItemsQuantity = (Math.floor(itemListChilds.length * 0.5));
+let hideItems = itemListChilds.slice (-(hideItemsQuantity));
+
+
+
+hideItems.forEach(item => {
+	item.classList.add('hide-items');
+});
+
+btn.addEventListener('click', function name() 
+{
+	hideItems.forEach(item => {
+		item.classList.toggle('hide-items--active');
+	});
+
+	if (btn.innerHTML === "показать еще") {
+		btn.innerHTML = "скрыть";
+	  } else {
+		btn.innerHTML = "показать еще";
+	  }
+
+})
+
+});
+
+
+
+// Модальные окна
+
+const modalBtn = document.querySelectorAll('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay ');
+const modals = document.querySelectorAll('.modal');
+const closeModalBtn = document.querySelectorAll('.close__modal');
+
+
+
+closeModalBtn.forEach(e => {
+	e.addEventListener('click',closeModal);
+});
+
+
+function closeModal() {
+
+modals.forEach((el) => {
+	el.classList.remove('modal--visible');
+});
+modalOverlay.classList.remove('modal-overlay--visible');
+
+document.querySelector('.body').style.cssText = `
+overflow: auto;
+user-select: auto;
+`;
+
+};
+
+modalBtn.forEach((el) => {
+
+	el.addEventListener('click' , (e) => {
+
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+
+		modalOverlay.classList.remove('modal-overlay--visible');
+		let path = e.currentTarget.getAttribute('data-path');
+
+		document.querySelector('.body').style.cssText = `
+		overflow: hidden;
+		user-select: none;
+`;
+
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+		modalOverlay.classList.add('modal-overlay--visible');
+		
+
+	});
+
+});
+
+
+// патерн для поля инпут емаайла
+
+
+const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// Проверка инпута первого модального окна
+
+const EmailInput = document.querySelector('.modal__subscribe-input');
+
+EmailInput.addEventListener('input', checkValidEmail);
+
+const EmailSubmit = document.querySelector('.modal__subscribe-btn');
+
+
+function checkValidEmail() {
+
+	function ValidateEmail(EmailInput)
+	{
+	
+	if(EmailInput.value.match(emailPattern))
+	{
+	EmailInput.focus();
+	return true;
+	}
+	else
+	{
+	EmailInput.focus();
+	return false;
+	}
+	}
+
+	EmailInput.classList.remove('modal__subscribe-input--active');
+	EmailSubmit.classList.remove('modal__subscribe-btn--active');
+	EmailSubmit.setAttribute("disabled", true);
+
+	if (EmailInput.value.length > 5 && ValidateEmail(EmailInput)==true  ) 
+
+	{
+
+		EmailInput.classList.add('modal__subscribe-input--active');
+		EmailSubmit.classList.add('modal__subscribe-btn--active');
+		EmailSubmit.removeAttribute("disabled");
+	}
+
+
+};
+
+// Клик по кнопке отправить первый инпут
+
+EmailSubmit.addEventListener('click', hideInput);
+
+function hideInput ()
+{
+
+EmailInput.style.cssText=`
+visibility: hidden;
+user-select: none;
+`;
+EmailSubmit.style.cssText=`
+visibility: hidden;
+user-select: none;
+`;
+
+
+document.querySelector('.modal__subscribe-after-click').style.
+
+cssText=`
+visibility:  visible;
+opacity: 1;
+transform: translateX(0%) translateY(-50%);
+
+`;
+
+}
+
+// Проверка инпута остальных
+
+let EmailInputAnswer = document.querySelectorAll('.modal__answer-input');
+
+EmailInputAnswer.forEach(function (e) { e.addEventListener('input', checkValidAnswer); })
+
+let EmailSubmitAnswer = document.querySelectorAll('.modal__answer-btn');
+
+let form = document.querySelector('.modal__answer-input-wrapper');
+
+
+function checkValidAnswer(e) {
+
+	let currentElement = e.target;
+	let currentValue = e.target.value;
+	let btnSubmitAnswer = e.target.nextElementSibling;
+
+	if(emailPattern.test(currentValue) && e.target.value.length > 5 ){
+	  //if validchek ok
+
+	  currentElement.classList.add('modal__answer-input--active');
+	  btnSubmitAnswer.removeAttribute("disabled");
+	 btnSubmitAnswer.classList.add('modal__answer-btn--active');
+	 
+	}else{
+		currentElement.classList.remove('modal__answer-input--active');
+		btnSubmitAnswer.classList.remove('modal__answer-btn--active');
+		btnSubmitAnswer.setAttribute("disabled", true);
+	  //if not okey
+	}
+
+  };
+
+// после отправки
+
+EmailSubmitAnswer.forEach(function (e) { e.addEventListener('click',  hideInputAnswer); })
+
+  function hideInputAnswer (e)
+  {
+   let currentBtn = e.target;
+   let currentInput = e.target.previousElementSibling ;
+   let afterClickAnswer = e.target.nextElementSibling;
+
+
+   currentBtn.style.cssText=`
+	 visibility: hidden;
+	 user-select: none;
+	 `;
+	 
+	 currentInput.style.cssText=`
+	 visibility: hidden;
+	 user-select: none;
+	 `;
+	 
+	 afterClickAnswer.style.cssText=`
+	 visibility:  visible;
+	 opacity: 1;
+	 transform: translateX( 0%) ;
+     `;
+
+
+document.querySelectorAll('.modal__answer-personal').forEach(function (i){
+i.style.
+
+cssText=`
+opacity: 0;
+pointer-events: none;
+user-select: none;
+`;
+
+	 });
+
+  };
+
+
+// Замена стандартной ошибки  вывода не коректного емайл 
+
+let email = document.querySelectorAll('[type = "email"]');
+
+email.forEach(function (e) {
+
+	e.addEventListener("input", function (event) {
+		if (e.validity.typeMismatch) {
+			e.setCustomValidity("не коректный email");
+		} else {
+			e.setCustomValidity("");
+		}
+	});
+});
+
+const callBackInput = document.querySelectorAll ('.modal__call-back-input');
+const callBackBtn = document.querySelector ('.modal__call-back-btn');
+
+callBackInput.forEach(e => {
+
+e.addEventListener('input', checkValidForm);
+
+});
+
+const PhonePatern = (/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/);
+
+function checkValidForm() {
+
+let validPhone = false;
+
+		if(	PhonePatern.test(callBackInput[0].value) )
+{
+callBackInput[0].classList.add('modal__call-back-input--active');
+validPhone = true;
+}
+else
+{
+
+callBackInput[0].classList.remove('modal__call-back-input--active');
+validPhone = false;
+}
+
+
+if(	callBackInput[1].value.length > 0 )
+{
+
+	
+callBackInput[1].classList.add('modal__call-back-input--active');
+validName = true;
+}
+else
+{
+callBackInput[1].classList.remove('modal__call-back-input--active');
+validName = false;
+}
+
+
+if (validName == true && validPhone == true) {
+
+
+	callBackBtn.removeAttribute("disabled");
+	callBackBtn.classList.add('modal__answer-btn--active');
+}
+else{
+
+	callBackBtn.classList.remove('modal__answer-btn--active');
+	callBackBtn.setAttribute("disabled", true);
+}
+};
+
+
+
+callBackBtn.addEventListener('click', hideInputs); 
+
+  function hideInputs (e)
+  {
+
+ const afterClickMesage = document.querySelector ('.modal__call-back-after-click');
+
+callBackBtn.style.cssText=`
+	 visibility: hidden;
+	 user-select: none;
+	 `;
+	 
+	 callBackInput.forEach(e => {
+		e.style.cssText=`
+		visibility: hidden;
+		user-select: none;
+		`;
+	 });
+	
+	 
+	 afterClickMesage.style.cssText=`
+	 visibility:  visible;
+	 opacity: 1;
+	 transform: translateX( 0%) ;
+     `;
+
+
+document.querySelector('.modal__personal-call-back').style.
+
+cssText=`
+opacity: 0;
+pointer-events: none;
+user-select: none;
+`;
+
+
+  };
+
+
+
+
+// модальные окна end
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,176 @@
-'use strict';
-
-const gulp = require('gulp');
-const watchSass = require('gulp-sass');
+const {src,dest,watch,series} = require('gulp');
+const scss = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const sync = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const fileinclude = require('gulp-file-include');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const fs = require('fs');
 const del = require('del');
-// для сжатия картинок
-const imagemin    = require('gulp-imagemin');
-const cache    = require('gulp-cache');
-const pngquant    = require('imagemin-pngquant');
-// для сжатия картинок end
+const cssnano = require('gulp-cssnano');
+const validator = require('gulp-w3c-html-validator');
+const formatHtml = require('gulp-format-html');
+const removeComments = require('gulp-strip-css-comments'); 
+
+const cache = require('gulp-cache');
 
 
 
-gulp.task('browserSync', function () {
-	browserSync.init({
-		server: {
-			baseDir: 'app'
-		},
-	})
-});
+// Собирает все файлы html в страницы
 
-gulp.task('watchHtml', function () {
-	return gulp.src('app/**/*.html')
-		.pipe(browserSync.reload({ stream: true }))
-});
-
-
-watchSass.compiler = require('node-sass');
-gulp.task('watchSass', function () {
-	return gulp.src('app/sass/**/*.scss')
-		.pipe(watchSass().on('error', watchSass.logError))
-		.pipe(gulp.dest('app/css'))
-// автопрефиксы
-		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) 
-		.pipe(gulp.dest('app/css')) 
-		.pipe(browserSync.reload({stream: true}));
-// автопрефиксы end
-});
+const htmlInclude = () => {
+    return src([
+        'src/html/*.html',
+    ])
+    .pipe(fileinclude({
+        prefix: '@',
+        basepath: '@file',
+    }))
+    .pipe(validator())
+    .pipe(formatHtml())
+    .pipe(dest('app/'))
+    .pipe(sync.stream());
+}
 
 
-gulp.task('watchJavaScript', function() {
-	return gulp.src([ 'app/js/**/*.js'])
-	.pipe(browserSync.reload({ stream: true }))
-});
+// Конвертирует scss в css
 
-//  сжатие картинок 
+const translateScss = () => {
+    return src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(scss({
+        outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer({
+        overrideBrowserslist: 'last 10 versions'
+    }))
+    .pipe(rename({
+        basename: "main",
+    }))
+    .pipe(dest('app/css'))
+    .pipe(rename({
+        suffix: ".min",
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('app/css'))
+    .pipe(sync.stream());
+}
 
-gulp.task('img', function () {
-	return gulp.src('app/img/**/*') 
-		.pipe(cache(imagemin({ 
-			
-			interlaced: true,
-			progressive: true,
-			svgoPlugins: [{ removeViewBox: false }],
-			use: [pngquant()]
-		})))
-		.pipe(gulp.dest('dist/img')); 
-});
+// переносим картинки
 
-//  сжатие картинок end
+const imagesCompress = () => {
+    return src('src/img/**/*.*')
+    .pipe(dest('app/img'))
+}
 
+// Работа с шрифтами
 
-gulp.task('clean', async function() {
-	return del.sync('dist'); // Удаляем папку dist перед сборкой
-});
-
-	// Удалть папку dist перед сборкой
-
-
-gulp.task('watch', function () {
-
-	gulp.watch('app/**/*.html', gulp.parallel('watchHtml')); 
-	gulp.watch('app/sass/**/*.scss', gulp.parallel('watchSass')); 
-	gulp.watch('app/**/*.js', gulp.parallel('watchJavaScript')); 
-
-});
-
-gulp.task('default', gulp.parallel( 'browserSync','watch'));
-
-// для начала работы пиши gulp 
+const  delFolder = async () => {
+    del.sync('app/fonts')
+}
 
 
+const fonts = async () => {
+
+    del.sync('app/fonts');
+
+    const srcFonts = 'src/fonts/';
+    const generateScssFonts = 'src/scss/helpers/_fonts.scss';
+
+    // форматирование в woff
+    src('src/fonts/**/*.ttf')
+    .pipe(ttf2woff())
+    .pipe(dest('app/fonts'));
+
+    // форматирование в woff2
+    src('src/fonts/**/*.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'));
+
+    // добавление шрифтов в fonts.scss
+
+    const checkWeight = {
+        black: 900,
+        extrabold: 800,
+        bold: 700,
+        semibold: 600,
+        medium: 500,
+        regular: 400,
+        light: 300,
+        thin: 100
+    }
+
+    fs.writeFile(generateScssFonts, "",()=>{});
+
+    fs.readdir(srcFonts,(err,items) =>{
+        items.forEach((item)=>{
+            const font_url = item.split('.')[0];
+            const font_name = item.split('-')[0];
+            let font_weight  = item.split('-')[1].split('.')[0].toLowerCase();
+            let font_style  = item.split('-')[1].split('.')[0].toLowerCase();
+            if (font_weight.includes('italic')) {
+                font_weight = font_weight.split('italic')[0];
+            }
+            if (font_weight === '') {
+                font_weight = 'regular';
+            }
+            if (font_style.includes('italic')) {
+                font_style = 'italic';
+            }
+            else {
+                font_style = 'normal';
+            }
+            fs.appendFileSync(generateScssFonts, `@include font("${font_url}","${font_name}", ${checkWeight[font_weight]}, "${font_style}"); \n`);
+        });
+    });
+}
+
+// Перемещение и работа js файлов
 
 
-gulp.task('build', gulp.parallel( 'clean','img','watchSass', async function () {
 
-	gulp.src('app/pages/**/*.html').pipe(gulp.dest('dist/pages'));
-	gulp.src('app/css/**/*.css').pipe(gulp.dest('dist/css'));
-	gulp.src('app/js/**/*.js').pipe(gulp.dest('dist/js'));
-	gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 
-}));
+const translateJs = () => {
+    return src('./src/js/**/*.js')
+    .pipe(dest('app/js'))
+    .pipe(sync.stream());
+}
 
-// отправить проект в dist + сжать фото.  gulp build
+
+// Сервер и Отслеживание в изменениях файлов
+
+const watcher = () => {
+    sync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+    watch('src/scss/**/*.scss',translateScss);
+    watch('src/html/**/*.html',htmlInclude);
+    watch('src/img/**/*.*',imagesCompress);
+    watch('src/js/*.js',translateJs);
+
+}
+
+const build = () => {
+    return src('app/css/main.min.css')
+    .pipe(removeComments({
+        preserve: false,
+    }))
+    .pipe(dest('dist'))
+}
+
+exports.fonts = fonts;
+
+exports.translateScss = translateScss;
+exports.delFolder = delFolder;
+exports.build = build;
+
+// exports.imagesCompress = imagesCompress;
+
+
+
+exports.default = series(htmlInclude,translateScss,translateJs,watcher);
